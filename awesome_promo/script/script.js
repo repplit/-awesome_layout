00dbe85281e014fa3314c623323deaf72748b0e7
Konstantin Gramm <57257093+repplit@users.noreply.github.com>
2019-11-15 14:22:50 +0200

    Add files via upload

--- awesome_promo/script/script.js
+++ awesome_promo/script/script.js
@@ -1,90 +1,81 @@
 
+//Вствляем код украины для ввода номера
 let phone = document.querySelector('#phone');
       phone.addEventListener('focus', ()=>{
             phone.value = '+380';
 });
 
+// Запуск лоадера
 const startLoader = () =>{
       document.getElementById('loader').classList.add('active');
       document.getElementById('content').classList.add('disable');
 }
-// Сбор данных для отправки
-const createData = () => {
-      data = {};
-      data.name = validName(document.querySelector('#name').value);
-      data.phone = validPhone(document.querySelector('#phone').value);
-      data.region =  window.location.search.replace('=', '');
 
-      return data;
-}
-// Проверка на число
-const isNumber = (n) =>  !isNaN(parseFloat(n)) && !isNaN(n - 0);
+// Событие для запуска
+document.querySelector('#button').addEventListener('click', ()=>{
+      let messege = new Sender('http://httpbin.org/post');
+      messege.sendMessege();
+});
 
-// Проверка валидности номера
-const validPhone = (phone) => {
-      if(phone.length >= 10 && isNumber(phone)){
-            return phone.substr(-10);
-      }else if(phone.length < 10 ){
-            return false;
-      }
-};
-
-// Проверка валидности имени  
-const validName = (name) => (name.length > 2 && !isNumber(name)) ? name : false;
-
-// коструктор ошибок
-const createError = (status) =>{
-      let errorMessege = {
-            nameError: 'Недопустимое имя',
-            phoneFormatError: 'Неправильный формат телефона',
-            phoneError: 'Номер телефона уже зарегестрирован'
-      };
-
-      let errorArea = document.querySelector('#error-output');
-      errorArea.innerHTML = `<span>${errorMessege[status]}</span>`;
-
-      setTimeout(()=>{errorArea.innerHTML = ''}, 5000);
-};
-
-// Функция запроса на сервер
-const post = (url, data) =>{
-      return fetch(url,{
-            method: 'POST',
-            body: JSON.stringify(data),
-            headers: new Headers({
-                  'Content-Type': 'application/json'
-                }),
-      })
-      .then(response => response.json());
-}
-
-// Функция отпрвки данных 
-const sendData = async () =>{
-      let data = createData();
-      let url = 'http://httpbin.org/post'; // тестовый адрес
-      
-      if(!data.name){
-            createError('nameError');
-            return;
-      }else if(!data.phone){
-            createError('phoneFormatError');
-            return;
+class Sender {
+      constructor(url){
+            this.url = url;
       }
 
-      // Отправка и обработка ответа
-      post(url, data)
-            // обрабатываем результат вызова 
-            .then((data)=>{
+      createData(){
+            const isNumber = (n) =>  !isNaN(parseFloat(n)) && !isNaN(n - 0);
+            const getMarket = (market) =>  new URLSearchParams(window.location.search).get("market");
+            const validName = (name) => (name.length > 2 && !isNumber(name)) ? name : false;
+            const validPhone = (phone) => (phone.length >= 10 && isNumber(phone)) ?  phone.substr(-10) : false;
+            
+            let data = {
+                  name : validName(document.querySelector('#name').value),
+                  phone : validPhone(document.querySelector('#phone').value),
+                  region :  getMarket()
+            };
+
+            return data;
+      }
+
+      createError(status){
+            let errorMessege = {
+                  nameError: 'Недопустимое имя',
+                  phoneFormatError: 'Неправильный формат телефона',
+                  phoneError: 'Номер телефона уже зарегестрирован'
+            }
+
+            document.querySelector('#error-output').innerHTML = `<span>${errorMessege[status]}</span>`;
+            setTimeout(()=>{document.querySelector('#error-output').innerHTML = ''}, 5000);
+      }
+
+      async sendMessege(){
+            let data = this.createData();
+
+            if(!data.name){
+                  let name = document.getElementById('name');
+                  name.classList.add('error');
+                  setInterval(()=>{name.classList.remove('error')}, 2000);
+                  return this.createError('nameError'); 
+            }else if(!data.phone){ 
+                  let phone = document.getElementById('phone');
+                  phone.classList.add('error');
+                  setInterval(()=>{phone.classList.remove('error')}, 2000);
+                  return this.createError('phoneFormatError');
+            };
+
+            return fetch(this.url, {
+                  method: 'POST',
+                  body: JSON.stringify(data),
+                  headers: new Headers({
+                        'Content-Type': 'application/json'
+                        })
+            })
+            .then(response => response.json())
+            .then((data) => {
                   console.log(data);
                   startLoader();
             })
-            .catch(error => console.error(error))
+            .catch(error=> console.error(error));
+      }
 }
-
-let button = document.querySelector('#button');
-
-// Событие для запуска
-button.addEventListener('click', sendData);
-
-
 
